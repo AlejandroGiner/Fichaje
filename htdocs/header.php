d5837e7b77fcc68eb7d942b34b6018d11fd63397
Alejandro Giner <alejandroginerpaez@gmail.com>
2023-11-06 10:22:42 +0100

    navbar

--- htdocs/header.php
+++ htdocs/header.php
@@ -1,9 +1,8 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
             <ul class="navbar-nav">
-                <li class="nav-item"><a href="" class="nav-link active">Link 1</a></li>
-                <li class="nav-item"><a href="" class="nav-link">Link 2</a></li>
-                <li class="nav-item"><a href="" class="nav-link">Link 3</a></li>
+                <li class="nav-item"><a href="/scripts/empleados/" class="nav-link">Empleados</a></li>
+                <li class="nav-item"><a href="/scripts/deptos/" class="nav-link">Departamentos</a></li>
             </ul>
         </div>
     </nav>
