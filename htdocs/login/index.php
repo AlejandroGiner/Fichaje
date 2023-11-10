1576328b0db90c8ccaae32241382530cea6ce442
Alejandro Giner <alejandroginerpaez@gmail.com>
2023-11-10 12:31:20 +0100

    no double login

--- htdocs/login/index.php
+++ htdocs/login/index.php
@@ -1,3 +1,13 @@
+<?php
+//Inicio la sesión
+session_start();
+
+//COMPRUEBA QUE EL USUARIO ESTA AUTENTIFICADO
+if(isset($_SESSION['username'])){
+    //si no existe, envio a la página de autentificacion
+    header("Location: /");
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
