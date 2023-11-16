a53aadd8575ab2166c6bc88d923826c5f88a4e18
Alejandro Giner <alejandroginerpaez@gmail.com>
2023-11-16 14:15:15 +0100

    login card

--- htdocs/login/index.php
+++ htdocs/login/index.php
@@ -16,48 +16,61 @@ if(isset($_SESSION['username'])){
     <title>Fichajes</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
 </head>
 <body>
-    
-    <div class="container-sm border">
-        <div class="panel panel-primary">
-            <div class="panel-heading text-center"><h2>Fichaje</h2></div>
+<section class="vh-100 bg-secondary">
+    <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col col-xl-10">
+                <div class="card" style="border-radius: 1rem;">
+                    <div class="card-body p-4 p-lg-5 text-black">
+                        <form class="container border pt-5 pb-5 " action="/control.php" method="POST">
+                            <div class="d-flex align-items-center mb-3 pb-1">
+                                <i class="bi bi-clock-history me-3" style="font-size: 5rem;"></i>
+                                <span class="h1 fw-bold mb-0">Cuándo libro</span>
+                            </div>
 
-            <h1>Iniciar sesión</h1>
-            <form class="container border pt-5 pb-5 " action="/control.php" method="POST">
-                <?php
-                    if (array_key_exists('login_error',$_GET)){ ?>
-                    <div class="alert alert-danger alert-dismissible" role="alert">
-                        Usuario o contraseña incorrectos.
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Inicio de sesión</h5>
+
+                                            <?php
+                                                if (array_key_exists('login_error',$_GET)){ ?>
+                                                <div class="alert alert-danger alert-dismissible" role="alert">
+                                                    Usuario o contraseña incorrectos.
+                                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                                </div>
+                                            <?php } ?>
+                                            <?php
+                                                if (array_key_exists('session_expired',$_GET)){ ?>
+                                                <div class="alert alert-danger alert-dismissible" role="alert">
+                                                    Tu sesión ha caducado.
+                                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                                                </div>
+                                            <?php } ?>
+                                            <div class="mb-3">
+                                                <label for="username" class="form-label">Nombre de usuario</label>
+                                                <input type="text" class="form-control form-control-lg" id="username" name="username">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="passwd" class="form-label">Contraseña</label>
+                                                <input type="password" class="form-control form-control-lg" id="passwd" name="passwd">
+                                            </div>
+                                            <div class="mb-3 form-check">
+                                                <input type="checkbox" class="form-check-input" id="keepSession">
+                                                <label class="form-check-label" for="keepSession">Mantener sesión iniciada</label>
+                                            </div>
+                                            <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+
+                            
+                        </form>
                     </div>
-                <?php } ?>
-                <?php
-                    if (array_key_exists('session_expired',$_GET)){ ?>
-                    <div class="alert alert-danger alert-dismissible" role="alert">
-                        Tu sesión ha caducado.
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                <?php } ?>
-                <div class="mb-3">
-                    <label for="username" class="form-label">Nombre de usuario</label>
-                    <input type="text" class="form-control form-control-lg" id="username" name="username">
                 </div>
-                <div class="mb-3">
-                    <label for="passwd" class="form-label">Contraseña</label>
-                    <input type="password" class="form-control form-control-lg" id="passwd" name="passwd">
-                </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="keepSession">
-                    <label class="form-check-label" for="keepSession">Mantener sesión iniciada</label>
-                </div>
-                <button type="submit" class="btn btn-primary">Iniciar sesión</button>
-            </form>
+            </div>
         </div>
     </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-
+</section>
 </body>
 </html>
