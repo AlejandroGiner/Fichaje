34059d693e2b9c38d539697286e7d03348ae8389
Alejandro Giner <alejandroginerpaez@gmail.com>
2023-12-18 18:17:11 +0100

    remove commented code

--- htdocs/header.php
+++ htdocs/header.php
@@ -14,19 +14,6 @@
         <a href="/scripts/turnos_publicados" class='nav-link px-5 <?php if(str_contains($_SERVER['PHP_SELF'],'turnos_publicados')){print('active');}?>'>
           <i class='bi-calendar-week d-block mb-1 text-center d-none d-xl-block' style='font-size: 2rem;'></i>Turnos publicados
         </a>
-        <?php /*
-        <a href="/scripts/categorias" class="nav-link px-5 <?php if(str_contains($_SERVER['PHP_SELF'],'categorias')){print('active');}?>">
-          <i class="bi-diagram-2-fill d-block mb-1 text-center d-none d-xl-block" style="font-size: 2rem;"></i>Categorías
-        </a>
-
-        <a href="/scripts/departamentos" class="nav-link px-5 <?php if(str_contains($_SERVER['PHP_SELF'],'departamentos')){print('active');}?>">
-          <i class="bi-grid d-block mb-1 text-center d-none d-xl-block" style="font-size: 2rem;"></i>Departamentos
-        </a>
-
-        <a href="/scripts/empleados" class="nav-link px-5 <?php if(str_contains($_SERVER['PHP_SELF'],'empleados')){print('active');}?>">
-          <i class="bi-people-fill d-block mb-1 text-center d-none d-xl-block" style="font-size: 2rem;"></i>Empleados
-        </a>
-        */ ?>
         <?php if($_SESSION['tipo_empleado'] == 3) { ?>
         <div class="nav-item dropdown">
           <a href="#" class="nav-link dropdown-toggle px-5" role="button" data-bs-toggle="dropdown" aria-expanded="false">
