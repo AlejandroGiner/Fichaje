721a66bdbda6d5b3fad2ebe9ef1031e6b25b2f29
Alejandro Giner <alejandroginerpaez@gmail.com>
2023-12-18 10:32:49 +0100

    add roles

--- htdocs/control.php
+++ htdocs/control.php
@@ -4,25 +4,33 @@ session_start();
 //vemos si el usuario y contraseña es válido
 include_once($_SERVER['DOCUMENT_ROOT']."/scripts/conn.php");
 
-$query = "SELECT password FROM usuario_web WHERE username=?";
-$stmt = $conn->prepare($query);
+$password_query = "SELECT password FROM usuario_web WHERE username=?";
+$stmt = $conn->prepare($password_query);
 $stmt->bind_param('s',$_POST["username"]);
 $stmt->execute();
 $stmt->bind_result($stored_hash);
 $stmt->fetch();
 $stmt->close();
-$conn->close();
 
 if ($stored_hash!=null && password_verify($_POST['passwd'],$stored_hash)){
     //usuario y contraseña válidos
     $_SESSION['username']= $_POST["username"];
     $_SESSION['last_login'] = date('Y-n-j H:i:s');
-    }
-
-if (isset($_SESSION['username'])){
-    header ("Location: /");
-} else {
+    
+    $data_query = 'SELECT e.id_tipo_empleado, e.nombre FROM usuario_web u JOIN empleado e ON (u.id_empleado=e.dni) WHERE u.username=?';
+    $stmt = $conn->prepare($data_query);
+    $stmt->bind_param('s',$_POST['username']);
+    $stmt->execute();
+    $stmt->bind_result($_SESSION['tipo_empleado'], $_SESSION['nombre']);
+    $stmt->fetch();
+    $stmt->close();
+    $conn->close();
+    
+    header("Location: /");
+}
+else {
     //si no existe le mando otra vez a la portada
+    $conn->close();
     header("Location: /login?login_error");
 }
 ?>
--- htdocs/header.php
+++ htdocs/header.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-xl navbar-dark bg-dark ">
   <div class="container-fluid">
     <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-decoration-none navbar-brand">
-      <i class="bi bi-clock-history px-5" style="font-size: 2rem;"></i>
+      <i class="bi bi-clock-history px-5" style="font-size: 4rem;"></i>Hola, <?php print($_SESSION['nombre']); ?>
     </a>
 
     <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,24 +27,25 @@
           <i class="bi-people-fill d-block mb-1 text-center d-none d-xl-block" style="font-size: 2rem;"></i>Empleados
         </a>
         */ ?>
+        <?php if($_SESSION['tipo_empleado'] == 3) { ?>
         <div class="nav-item dropdown">
           <a href="#" class="nav-link dropdown-toggle px-5" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             <i class="bi-building-fill-gear d-block mb-1 text-center d-none d-xl-block" style="font-size: 2rem;"></i>Administración
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item <?php if(str_contains($_SERVER['PHP_SELF'],'empleados/')){print('active');}?>" href="/scripts/empleados">
+            <li><a class="dropdown-item <?php if(str_contains($_SERVER['PHP_SELF'],'empleados/')){print('active');}?>" href="/admin/empleados">
               <i class="bi-people-fill"></i> Empleados
             </a></li>
-            <li><a class="dropdown-item <?php if(str_contains($_SERVER['PHP_SELF'],'turnos/')){print('active');}?>" href="/scripts/turnos">
+            <li><a class="dropdown-item <?php if(str_contains($_SERVER['PHP_SELF'],'turnos/')){print('active');}?>" href="/admin/turnos">
               <i class="bi-clock-fill"></i> Turnos
             </a></li>
-            <li><a class="dropdown-item <?php if(str_contains($_SERVER['PHP_SELF'],'departamentos/')){print('active');}?>" href="/scripts/departamentos">
+            <li><a class="dropdown-item <?php if(str_contains($_SERVER['PHP_SELF'],'departamentos/')){print('active');}?>" href="/admin/departamentos">
             <i class="bi-grid"></i> Departamentos
             </a></li>
-            <li><a class="dropdown-item <?php if(str_contains($_SERVER['PHP_SELF'],'categorias/')){print('active');}?>" href="/scripts/categorias">
+            <li><a class="dropdown-item <?php if(str_contains($_SERVER['PHP_SELF'],'categorias/')){print('active');}?>" href="/admin/categorias">
             <i class="bi-diagram-2-fill"></i> Categorías
             </a></li>
-            <li><a class="dropdown-item <?php if(str_contains($_SERVER['PHP_SELF'],'register/')){print('active');}?>" href="/scripts/register">
+            <li><a class="dropdown-item <?php if(str_contains($_SERVER['PHP_SELF'],'register/')){print('active');}?>" href="/admin/register">
               <i class="bi-person-fill-add"></i> Registrar usuario
             </a></li>
             <li><a class="dropdown-item" href="/scripts/horas/get_horas.php">
@@ -52,6 +53,7 @@
             </a></li>
           </ul>
         </div>
+        <?php } ?>
           
         <a href="/logout.php" class="nav-link px-5">
           <i class="bi-door-closed d-block mb-1 text-center d-none d-xl-block" style="font-size: 2rem;"></i>Cerrar sesión
