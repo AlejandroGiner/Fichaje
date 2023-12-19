0df8bf372f9faaa0ee78bfe922a6ca3bcbb724af
Alejandro Giner <alejandroginerpaez@gmail.com>
2023-12-19 04:13:10 +0100

    remove wip

--- htdocs/index.php
+++ htdocs/index.php
@@ -26,8 +26,6 @@
     -->
     <img src="/img/bg-<?php print('f') ?>.png" class="img-fluid">
 
-<?php include($_SERVER['DOCUMENT_ROOT'].'/darkmode.php'); ?>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
