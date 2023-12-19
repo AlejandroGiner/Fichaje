411b16f961acbb5d5b4a8541fb3c54ce662ee4cd
Alejandro Giner <alejandroginerpaez@gmail.com>
2023-12-19 11:56:15 +0100

    qr codes

---
+++ htdocs/qr/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Códigos QR</title>
+    <?php include($_SERVER['DOCUMENT_ROOT']."/header.php"); ?>
+
+</head>
+<body>
+    <div class="container container-fluid text-center mt-5 pt-5">
+        <div class="row d-flex align-items-center">
+            <div class="col">
+                <div class="card">
+                    <img src="/img/cuandolibro_qr.png" class="card-img-top">
+                    <div class="card-body">
+                        <h1 class="card-title">Sitio web</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+            <div class="card">
+                    <img src="/img/github_qr.png" class="card-img-top">
+                    <div class="card-body">
+                        <h1 class="card-title">Github</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
