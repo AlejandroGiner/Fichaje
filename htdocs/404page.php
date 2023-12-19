38c48cdad587b905b2a990e0472dd75c1d56b72e
Alejandro Giner <alejandroginerpaez@gmail.com>
2023-12-19 12:22:09 +0100

    404 improvements

---
+++ htdocs/404page.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include($_SERVER['DOCUMENT_ROOT']."/header.php"); ?>
+</head>
+<body>
+    <?php
+        include($_SERVER['DOCUMENT_ROOT']."/navbar.php");
+    ?>
+    <h1>Error 404</h1>
+    <a href="/">Volver a página de inicio</a>
+</body>
+</html>
